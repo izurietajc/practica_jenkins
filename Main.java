7397b404e8aaf6138d90f1a3dce366c77f5e6d91
Juan Carlos Izurieta <izurietajc@gmail.com>
2020-09-24 20:51:08 -0300

    Nuevo cambio

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo, desde Java");
+		System.out.println("La conexión con Git-Hub se ha hecho");
 	}
 	
 }
