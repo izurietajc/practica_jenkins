0e4e7bfc8d4e401df870296a0177f8c0e0769589
Juan Carlos Izurieta <izurietajc@gmail.com>
2020-09-27 01:44:58 -0300

    Hola mundo nro 2

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, desde Java");
-		System.out.println("La conexión con Git-Hub se ha hecho");
-		System.out.println("Ahora se puede descargar desde Jenkins");
+		System.out.println("Hola mundo número 2");
 	}
 	
 }
