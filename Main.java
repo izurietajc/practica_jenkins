5bb47b646be3e939a8d1389d83b44067f2b0782f
Juan Carlos Izurieta <izurietajc@gmail.com>
2020-09-27 03:55:07 -0300

    Ahora es un sitio web

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-	
-	public static void main(String[] args) {
-		System.out.println("Hola mundo número 3");
-	}
-	
-}
---
+++ index.jsp
@@ -0,0 +1,7 @@
+<%page language="Java" %>
+
+<html>
+	<body>
+		Hola a todos
+	</body>
+</html>
